e246f228156c7a3922267c510e05082893758d97
Umakant Dwivedi <78226651+ukdgit@users.noreply.github.com>
2022-12-29 19:24:43 +0530

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,16 +1,16 @@
 // arr = array of numbers
 function major(arr)
 {
-  const majorityElement = (arr = []) => {
-   const threshold = Math.floor(arr.length / 2);
-   const map = {};
-   for (let i = 0; i < arr.length; i++) {
-      const value = arr[i];
-      map[value] = map[value] + 1 || 1;
-      if (map[value] > threshold)
-         return value
-   };
-   return false;
+//   const majorityElement = (arr = []) => {
+//    const threshold = Math.floor(arr.length / 2);
+//    const map = {};
+//    for (let i = 0; i < arr.length; i++) {
+//       const value = arr[i];
+//       map[value] = map[value] + 1 || 1;
+//       if (map[value] > threshold)
+//          return value
+//    };
+//    return false;
 };
 //console.log(majorityElement(arr));
 }
