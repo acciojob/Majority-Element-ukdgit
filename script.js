779a64af3a10d619cd513728a816c6af5623e4e9
Umakant Dwivedi <78226651+ukdgit@users.noreply.github.com>
2023-02-09 15:00:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-
+ function findMajority(arr)
+    {
+        let map = new Map();
+ 
+        for(let i = 0; i < arr.length; i++) {
+            if (map.has(arr[i])) {
+                    let count = map.get(arr[i]) +1;
+                    if (count > arr.length /2) {
+                        document.write("Majority found :- " + arr[i]);
+                        return;
+                    } else
+                        map.set(arr[i], count);
+ 
+            }
+            else
+                map.set(arr[i],1);
+            }
+            document.write(" No Majority element");
+    }
